c61cff674dffedf8538a68079094462a79c1bc95
Rodrigo Melo <upperapps@gmail.com>
2019-06-29 14:03:17 -0500

    Initial commit

---
+++ src/main/kotlin/com/upperapps/sakila/addressservice/SakilaAddressServiceApplication.kt
@@ -0,0 +1,11 @@
+package com.upperapps.sakila.addressservice
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SakilaAddressServiceApplication
+
+fun main(args: Array<String>) {
+	runApplication<SakilaAddressServiceApplication>(*args)
+}
---
+++ src/test/kotlin/com/upperapps/sakila/addressservice/SakilaAddressServiceApplicationTests.kt
@@ -0,0 +1,16 @@
+package com.upperapps.sakila.addressservice
+
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.junit4.SpringRunner
+
+@RunWith(SpringRunner::class)
+@SpringBootTest
+class SakilaAddressServiceApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
